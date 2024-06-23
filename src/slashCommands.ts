29f95962c34ef425d9380cb7c5538ceefb410fb4
ckybb <kiiyamans.k138@gmail.com>
2024-06-23 12:44:43 +0900

    fix import/export bug

--- src/slashCommands.ts
+++ src/slashCommands.ts
@@ -1,6 +1,6 @@
 import {ping} from "./commands/ping";
 import {join} from "./commands/join";
 import {leave} from "./commands/leave";
-import { joinparrot } from "./commands/joinparrot";
+import { joinparrot } from "./commands/joinParrot";
 
 export const commands = [ping, join, joinparrot, leave];
